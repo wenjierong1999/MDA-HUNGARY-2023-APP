4e759086c7b0a3110b873181830f6eca90812470
Jierong Wen <wenjierong1999@gmail.com>
2023-06-03 17:12:57 +0200

    update

--- src/app.py
+++ src/app.py
@@ -10,6 +10,7 @@ app = dash.Dash(__name__,
                 title='MDA Project',
                 use_pages=True,
                 external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
 
 ########################################################################################################################
 #                                            DATA LOADING                                                 #
